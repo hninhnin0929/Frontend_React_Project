0baf523e8776815c5f9b6dbdab7dd72e05fe1164
unknown <hninhnin0929>
2022-12-10 22:29:55 +0630

    first commit

--- src/form/FormDemo.js
+++ src/form/FormDemo.js
@@ -6,20 +6,76 @@ class FormDemo extends React.Component
     {
         super(props);
         this.state = {
-            name : ""
+            title : "",
+            description : "",
+            genre : ""
         }
+        this.handleTitleChange = this.handleTitleChange.bind(this);   //bind with current obj
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handleGenreChange = this.handleGenreChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    // nameCh
+    handleTitleChange(event)
+    {
+        console.log("Event ", event.target.value);
+        console.log("this ", this);     //undefiend
+        this.setState({
+            title : event.target.value
+        })
+    }
+    handleDescriptionChange(event)
+    {
+        console.log("Event ", event.target.value);
+        console.log("this ", this);     //undefiend
+        this.setState({
+            description : event.target.value
+        })
+    }
+    handleGenreChange(event)
+    {
+        console.log("Event ", event.target.value);
+        console.log("this ", this);     //undefiend
+        this.setState({
+            genre : event.target.value
+        })
+    }
+    handleSubmit(event){
+        console.log("Form submit", this.state);
+        event.preventDefault();//not to reload
+        
+    }
     render()
     {
         return(
             <div className="container col-6">
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="mb-3">
-                        <label className="form-label">
+                        <label htmlFor="name" className="form-label">
                             Name:                            
                         </label>
-                        <input type="text" name="name" className="form-control"/>
+                        <input type="text" name="name" className="form-control"
+                        value={this.state.title}
+                        onChange={(event)=>this.handleTitleChange(event)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">
+                            Description:                            
+                        </label>
+                        <textarea id="description" className="form-control"
+                        value={this.state.description}
+                        onChange={(event)=>this.handleDescriptionChange(event)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="genre" className="form-label">
+                        Genre:                            
+                        </label>
+                        <select id="genre" className="form-control"
+                        value={this.state.genre} onChange={(event)=>this.handleGenreChange(event)}>
+                            <option>Si-Fi</option>
+                            <option>Crime</option>
+                            <option selected>Drama</option>
+                            <option>Rumor</option>
+                        </select>
                     </div>
                     <div className="">
                         <input type="submit" className="btn btn-primary" value="Submit"></input>
